e5b93bd980d2607b9bbbc1b59beb14864bf34011
László Király <laszlo.t.kiraly@gmail.com>
2021-09-26 22:07:29 +0200

    maci

--- gollum.sh
+++ gollum.sh
@@ -3,6 +3,8 @@
 PROJECT_NAME="${PWD##*/}"
 DOCKER_IMAGE="gollumorg/gollum:latest"
 
+open -a Safari "http://localhost:4567"
+
 docker run --rm -it --name $PROJECT_NAME \
     -v $PWD:/wiki \
     -p 4567:4567 \
